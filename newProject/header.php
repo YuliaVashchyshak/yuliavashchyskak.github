d309c3aaebd8231cb5e3adf87aea6ece0097d6b1
Yulia Vashchyshak <yuliavashchyshak@gmail.com>
2023-08-29 17:31:05 +0300

    aad template for new project

---
+++ newProject/header.php
@@ -0,0 +1,16 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title><?php bloginfo('description') ?></title>
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+     <?php wp_head(); ?>
+ </head>
+
+ <body>
+    
