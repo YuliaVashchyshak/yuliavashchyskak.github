c5cc17b96fd9983f607d87ff267c016492344d45
Yulia Vashchyshak <yuliavashchyshak@gmail.com>
2023-11-07 11:40:55 +0200

    delete

--- Bus/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const burgerBtn = document.querySelector('.header__burger-btn')
-const burgerMenu = document.querySelector('.header__burger-wrapper')
-burgerBtn.addEventListener('click', () => {
-    burgerMenu.classList.toggle('open')
-    burgerBtn.classList.toggle('open')
-})
